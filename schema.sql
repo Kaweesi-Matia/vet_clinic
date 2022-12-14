40e4f415f36830b75d91a4c3fb3ed48d105ed3cf
Kaweesi Matia <matiakaweesi@gmail.com>
2022-12-14 14:04:49 +0300

    add updates to the database

--- schema.sql
+++ schema.sql
@@ -5,3 +5,5 @@ CREATE TABLE animals (
 );
  CREATE TABLE Animals ( id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL ,date_of_birth DATE NOT NULL ,escape_attempts 
 INT NOT NULL, neutered BOOL NOT NULL,weight_kg DECIMAL NOT NULL);
+
+ ALTER TABLE Animals ADD COLUMN SPECIES TEXT ;
